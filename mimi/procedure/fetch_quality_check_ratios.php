ffa0e8bad709e06953da3af1b2e111470df5ba25
mimipieper <mimipieper04@gmail.com>
2023-11-27 16:02:04 -0800

    procedure working

--- mimi/procedure/fetch_quality_check_ratios.php
+++ mimi/procedure/fetch_quality_check_ratios.php
@@ -1,17 +1,23 @@
 <?php
+// No user input :)
+// As such, this code is not vulnerable to SQL injection attacks
 
+// load database configuration
 require_once '/home/SOU/pieperm/dbconfig.php'; 
 
+// configure error reporting
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 ini_set('display_errors', '1');
 
 $dbconnect = new mysqli($hostname, $username, $password, $schema);
 
+// handle database connection errors
 if ($dbconnect->connect_error) {
     die("Database connection failed: " . $dbconnect->connect_error);
 }
 
-// HTML Header and CSS Link
+// HTML header and CSS link
+// being HTML output for the page
 echo '<html>';
 echo '<head>';
 echo '<title>Product Type Quality Check Ratios</title>';
@@ -19,22 +25,28 @@ echo '<link rel="stylesheet" type="text/css" href="style.css">';
 echo '</head>';
 echo '<body>';
 
-// Calling the stored procedure
+// call the stored procedure
 $result = $dbconnect->query("CALL p_GetQualityCheckRatio()");
 
 if ($result) {
+    // start the table structure to display the results
     echo "<table>";
     echo "<tr><th>Product Type</th><th>Pass/Fail Ratio</th></tr>";
+
+    // iterate through each row of results
+    // output each row as a table row
     while ($row = $result->fetch_assoc()) {
         echo "<tr><td>" . $row["Product Type"] . "</td><td>" . $row["Pass/Fail Ratio"] . "</td></tr>";
     }
     echo "</table>";
+    // end table structure
 } else {
     echo "No data found";
 }
 
 echo '</body>';
 echo '</html>';
+// end HTML output
 
 $dbconnect->close();
 ?>
