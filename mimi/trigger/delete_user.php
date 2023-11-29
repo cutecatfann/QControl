7d7aa0608544e1fb777288be65781e79e7cf7e4a
Mimi P <54961844+cutecatfann@users.noreply.github.com>
2023-11-29 13:34:42 -0800

    Update delete_user.php

--- mimi/trigger/delete_user.php
+++ mimi/trigger/delete_user.php
@@ -26,10 +26,26 @@ if (isset($_POST['submit'])) {
     $stmt->bind_param("s", $usr_name_to_delete);
 
     if (!$stmt->execute()) {
-        echo 'An error occurred. The user has not been deleted.';
-    } else {
-        echo "User deleted successfully.";
-    }
+            printf('An error occurred. Your data has not been submitted.  ');
+            die("Error: " . $dbconnect->error);
+        } else {
+            echo "User deleted successfully.<br> Below is the log of your most recent change. <br>";
+
+            // Query the latest_usr_audit_entry view
+            $result = $dbconnect->query("SELECT * FROM latest_usr_audit_entry");
+
+            if ($result->num_rows > 0) {
+                // Output data in a table
+                echo "<table><tr><th>Audit ID</th><th>User Name</th><th>Timestamp</th><th>User Role</th><th>User Email</th><th>Action Performed By</th><th>Action Type</th></tr>";
+                // fetch associative array
+                while($row = $result->fetch_assoc()) {
+                    echo "<tr><td>".$row["audit_id"]."</td><td>".$row["usr_name"]."</td><td>".$row["timestamp"]."</td><td>".$row["usr_role"]."</td><td>".$row["user_email"]."</td><td>".$row["action_performed_by"]."</td><td>".$row["action_type"]."</td></tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "0 results";
+            }
+        }
 
     $stmt->close();
 }
