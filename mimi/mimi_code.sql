e5e8d15b7c94533a6c5405439b260b7c4b366405
mimipieper <mimipieper04@gmail.com>
2023-12-04 13:51:45 -0800

    Comments on my code

--- mimi/mimi_code.sql
+++ mimi/mimi_code.sql
@@ -1,4 +1,15 @@
+/*
+Title: Mimi's MySQL Object Implementation
+Author: Mimi Pieper
+Date: 01/12/2023
+Description: This file contains the MySQL objects Mimi created for the QControl database. 
+
+It contains a View (v_userRole), Procedure (p_GetQualityCheckRatio), Function (f_AverageCheckValue), and Triggers (after_usr_insert, after_usr_update, after_usr_delete)
+*/
+
 -- View: See certain details about all users in the database
+-- Expected Output is a table with all current users, one example will be:
+-- User ID : 2 | Name : Mimi | Role : q_manager | Status : Active
 CREATE VIEW v_UserRole AS
 SELECT 
     usr_id AS UserID,
@@ -13,6 +24,12 @@ FROM usr;
 SELECT * from v_UserRole;
 
 -- Procedure
+-- The procedure is useful for assessing the overall quality performance of different product types. A higher pass/fail ratio indicates better quality control or fewer issues during production for that product type. It's useful for quality managers to monitor and improve the manufacturing process.
+
+-- Pass/Fail Ratio Calculation: This is a fraction. The numerator is the total count of batches with the status 'accepted' for each product type. The denominator is the total count of batches with the status 'rejected' for each product type, with a minimum value of 1 to avoid division by zero.
+
+-- Expected Output: You will see:
+-- Chocolate Chip | 1.0000
 CREATE DEFINER=`pieperm`@`%` PROCEDURE `p_GetQualityCheckRatio`()
 BEGIN
     SELECT 
@@ -28,6 +45,7 @@ BEGIN
 END
 
 -- Function: Calculates the average check value for a specific batch. This will show the quality level for a given batch.
+-- Expected Input/Output: Enter any Batch ID like 2, which will result in 0.564037. Any non used Batch IDs (example: 8387), will result in a value of 0.
 CREATE DEFINER=`pieperm`@`%` FUNCTION `f_AverageCheckValue`(batch_id_input INT) RETURNS float
     DETERMINISTIC
 BEGIN 
@@ -40,6 +58,9 @@ END
 
 -- Tigger: Three triggers logs all changes to the usr table into the usr_audit table
 -- After Insert:
+-- Example Input: User Name: Marvin, User Email: marvinlovesfish@gmail.com, Password: password, User Role: q_tech
+-- Example Output: You will see a log of the most recent insert with this success message: User added successfully.
+-- Here is an example output: 8 | Marvin | 2023-12-04 13:47:08	| q_tech |marvinlovesfish@gmail.com	| pieperm@localhost | INSERT
 CREATE DEFINER=`pieperm`@`%` TRIGGER after_usr_insert
 AFTER INSERT ON usr
 FOR EACH ROW
@@ -49,6 +70,9 @@ BEGIN
 END
 
 -- After Update:
+-- Example Input: Origial User Name: Marvin, New User Name: Marvin Pieper
+-- Example Output: You will see a log of the most recent insert with this success message: User updated successfully. Below is the log of your most recent change.
+-- 9 | Marvin Pieper | 2023-12-04 13:49:20 | q_tech | marvinlovesfish@gmail.com	| pieperm@localhost	| UPDATE
 CREATE DEFINER=`pieperm`@`%` TRIGGER after_usr_update
 AFTER UPDATE ON usr
 FOR EACH ROW
@@ -58,6 +82,9 @@ BEGIN
 END
 
 -- After Delete: 
+-- Example Input: Marvin
+-- Example Output: You will see a log of the most recent insert with this success message: User deleted successfully.Below is the log of your most recent change.
+-- 9 | Marvin Pieper | 2023-12-04 13:49:20 | q_tech | marvinlovesfish@gmail.com | pieperm@localhost | UPDATE
 CREATE DEFINER=`pieperm`@`%` TRIGGER after_usr_delete
 AFTER DELETE ON usr
 FOR EACH ROW
